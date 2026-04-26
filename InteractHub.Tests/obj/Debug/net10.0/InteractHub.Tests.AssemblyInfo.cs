14663a72ba0796d3f376ee222349942b88116fd2
Blue <nguyenblue1310@gmail.com>
2026-04-26 15:33:42 +0700

    s

--- InteractHub.Tests/obj/Debug/net10.0/InteractHub.Tests.AssemblyInfo.cs
+++ InteractHub.Tests/obj/Debug/net10.0/InteractHub.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InteractHub.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb9fc48ed33e0bfb06f2196e3da574bd5915567e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8813a76b6791c1299b464542bbc515021b460288")]
 [assembly: System.Reflection.AssemblyProductAttribute("InteractHub.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InteractHub.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
